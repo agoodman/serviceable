92a5b68b2f8a57aca37af9345765d0ada3889706
Aubrey <aubrey.goodman@gmail.com>
2013-08-08 11:12:29 -0400

    added support for filtering via query string

--- lib/serviceable.rb
+++ lib/serviceable.rb
@@ -82,15 +82,14 @@ module Serviceable
       # if an included association is present, only and except params can be nested
       # include[user][except]=encrypted_password
       # include[user][only][]=first_name&include[user][only][]=last_name
+      # include[user][only]=first_name,last_name
       define_method("merge_options") do |options={}|
         merged_options = options || {}
         for key in [:only, :except, :include]
           opts = {key => params[key]} if params[key]
           merged_options = merged_options.merge(opts) if opts
         end
-        puts "options before split: #{merged_options}"
         merged_options = deep_split(merged_options)
-        puts "options after split: #{merged_options}"
         return merged_options
       end
       
@@ -109,6 +108,15 @@ module Serviceable
         @instance = object.to_s.camelize.constantize.new(params[object])
       end
       
+      # query string params can be used to filter collections
+      #
+      # filters apply on associated collections using the following conventions:
+      # where[user][category]=Expert
+      # where[user][created_at][gt]=20130807T12:34:56.789Z
+      #
+      # filters can be constructed with AND and OR behavior
+      # where[tags][id][in]=123,234,345  (OR)
+      # where[tags][id]=123&where[tags][id]=234  (AND)
       define_method("assign_collection") do
         @collection = object.to_s.camelize.constantize
         if params[:include].kind_of?(Hash)
@@ -119,15 +127,41 @@ module Serviceable
         if params[:include].kind_of?(String)
           @collection = @collection.includes(params[:include].split(",").map(&:to_sym))
         end
-        for clause in (params[:where].keys rescue [])
-          puts "where #{clause} => #{params[:where][clause]}"
-          @collection = @collection.includes(clause).where(clause => params[:where][clause])
+        for assoc in (params[:where].keys rescue [])
+          attrs = params[:where][assoc]
+          if attrs.kind_of?(Hash)
+            for target_column in attrs.keys
+              op = :eq if attrs[target_column].kind_of?(String)
+              op ||= attrs[target_column].keys[0].to_sym
+              value = attrs[target_column] if op==:eq
+              value ||= is_time_column?(target_column) ? Time.parse(attrs[target_column][op]) : attrs[target_column][op]
+              unless assoc.to_sym==object.to_s.pluralize.to_sym
+                @collection = @collection.includes(assoc)
+              end
+              puts "op: #{op}"
+              if op==:gt
+                @collection = @collection.where("#{assoc}.#{target_column} > ?",value)
+              elsif op==:lt
+                @collection = @collection.where("#{assoc}.#{target_column} < ?",value)
+              elsif op==:in
+                @collection = @collection.where("#{assoc}.#{target_column} IN (?)",value)
+              elsif op==:eq
+                @collection = @collection.where(assoc => { target_column => value })
+              end
+            end
+          else
+            @collection = @collection.includes(assoc).where(assoc => attrs)
+          end
         end
       end
       
       # designed to traverse an entire hash, replacing delimited strings with arrays of symbols
       define_method("deep_split") do |hash={},pivot=','|
         Hash[hash.map {|k,v| [k.to_sym,v.kind_of?(String) ? v.split(pivot).map(&:to_sym) : (v.kind_of?(Hash) ? deep_split(v,pivot) : v)]}]
+      end
+      
+      define_method("is_time_column?") do |column|
+        !!column[-3,3]=='_at'
       end
     end
 
