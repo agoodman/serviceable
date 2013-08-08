12b49e282379759210dd3b01df72522a84d5f628
Aubrey <aubrey.goodman@gmail.com>
2013-08-07 21:38:20 -0400

    abandoned @#{object} naming; updated before filters to instance and collection; added count action

--- lib/serviceable.rb
+++ lib/serviceable.rb
@@ -15,50 +15,58 @@ module Serviceable
     #
     def acts_as_service(object,options={})
 
-      before_filter :assign_new_instance, :only => :create
-      before_filter :assign_existing_instance, :only => [ :show, :update, :destroy ]
-      before_filter :assign_instances, :only => :index
+      before_filter :assign_new_instance, only: :create
+      before_filter :assign_existing_instance, only: [ :show, :update, :destroy ]
+      before_filter :assign_collection, only: [ :index, :count ]
+      before_filter :did_assign_collection, only: [ :index, :count ]
 
       define_method("index") do
         respond_to do |format|
-          format.json { eval "render :json => @#{object.to_s.pluralize}.to_json(merge_options(options[:index]))" }
-          format.xml  { eval "render :xml => @#{object.to_s.pluralize}.to_xml(merge_options(options[:index]))" }
+          format.json { render json: @collection.to_json(merge_options(options[:index])) }
+          format.xml  { render xml: @collection.to_xml(merge_options(options[:index])) }
+        end
+      end
+      
+      define_method("count") do
+        respond_to do |format|
+          format.json { render json: @collection.count }
+          format.xml { render xml: @collection.count }
         end
       end
 
       define_method("create") do
         respond_to do |format|
-          if eval "@#{object}.save"
-            format.json { eval "render :json => @#{object}" }
-            format.xml  { eval "render :xml => @#{object}" }
+          if @instance.save
+            format.json { render json: @instance }
+            format.xml  { render xml: @instance }
           else
-            format.json { eval "render :json => { :errors => @#{object}.errors.full_messages }, :status => :unprocessable_entity" }
-            format.xml  { eval "render :xml => { :errors => @#{object}.errors.full_messages }, :status => :unprocessable_entity" }
+            format.json { render json: { errors: @instance.errors.full_messages }, status: :unprocessable_entity }
+            format.xml  { render xml: { errors: @instance.errors.full_messages }, status: :unprocessable_entity }
           end
         end
       end
 
       define_method("show") do
         respond_to do |format|
-          format.json { eval "render :json => @#{object}.to_json(options[:show])" }
-          format.xml  { eval "render :xml => @#{object}.to_xml(options[:show])" }
+          format.json { render json: @instance.to_json(merge_options(options[:show])) }
+          format.xml  { render xml: @instance.to_xml(merge_options(options[:show])) }
         end
       end
 
       define_method("update") do
         respond_to do |format|
-          if eval "@#{object}.update_attributes(params[object])"
+          if @instance.update_attributes(params[object])
             format.json { head :ok }
             format.xml  { head :ok }
           else
-            format.json { eval "render :json => { :errors => @#{object}.errors.full_messages }, :status => :unprocessable_entity" }
-            format.xml  { eval "render :xml => { :errors => @#{object}.errors.full_messages }, :status => :unprocessable_entity" }
+            format.json { render json: { errors: @instance.errors.full_messages }, status: :unprocessable_entity }
+            format.xml  { render xml: { errors: @instance.errors.full_messages }, status: :unprocessable_entity }
           end
         end
       end
 
       define_method("destroy") do
-        eval "@#{object}.destroy"
+        @instance.destroy
 
         respond_to do |format|
           format.json { head :no_content }
@@ -87,26 +95,39 @@ module Serviceable
       end
       
       define_method("assign_existing_instance") do
-        instance = object.to_s.camelize.constantize
-        instance = instance.includes(params[:include]) if params[:include]
-        instance = instance.find(params[:id])
-        eval "@#{object} = instance"
+        @instance = object.to_s.camelize.constantize
+        if params[:include].kind_of?(Hash)
+          @instance = @instance.includes(params[:include].keys)
+        end
+        if params[:include].kind_of?(String)
+          @instance = @instance.includes(params[:include].split(",").map(&:to_sym))
+        end
+        @instance = @instance.find(params[:id])
       end
       
       define_method("assign_new_instance") do
-        instance = object.to_s.camelize.constantize.new(params[object])
-        eval "@#{object} = instance"
+        @instance = object.to_s.camelize.constantize.new(params[object])
       end
       
-      define_method("assign_instances") do
-        collection = object.to_s.camelize.constantize
-        collection = collection.includes(params[:include]) if params[:include]
-        eval "@#{object.to_s.pluralize} = collection"
+      define_method("assign_collection") do
+        @collection = object.to_s.camelize.constantize
+        if params[:include].kind_of?(Hash)
+          for assoc in params[:include].keys
+            @collection = @collection.includes(assoc.to_sym)
+          end
+        end
+        if params[:include].kind_of?(String)
+          @collection = @collection.includes(params[:include].split(",").map(&:to_sym))
+        end
+        for clause in (params[:where].keys rescue [])
+          puts "where #{clause} => #{params[:where][clause]}"
+          @collection = @collection.includes(clause).where(clause => params[:where][clause])
+        end
       end
       
       # designed to traverse an entire hash, replacing delimited strings with arrays of symbols
       define_method("deep_split") do |hash={},pivot=','|
-        Hash[hash.map {|k,v| [k.to_sym,v.kind_of?(String) ? v.split(pivot).map(&:to_sym) : (v.kind_of?(Hash) || v.kind_of?(Array) ? deep_split(v,pivot) : v)]}]
+        Hash[hash.map {|k,v| [k.to_sym,v.kind_of?(String) ? v.split(pivot).map(&:to_sym) : (v.kind_of?(Hash) ? deep_split(v,pivot) : v)]}]
       end
     end
 
